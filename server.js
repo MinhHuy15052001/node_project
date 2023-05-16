6b421d02b84a4cf6085522dabe2223c0aeb5bfb1
MinhHuy1552001 <khuongminhhuy1505@gmail.com>
2023-05-17 00:35:07 +0700

    Book create index

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views',__dirname,'/views');
 app.set('layout','views/layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({limit : '10mb' , extended : false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -24,6 +27,8 @@ const db = mongoose.connect;
 // db.once('open', () => console.error('Connected to Mongoose'));
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
+app.use('/books',bookRouter);
 
 
 app.listen(process.env.PORT || 3000);
